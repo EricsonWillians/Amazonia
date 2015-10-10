2ebe8f657de0795b6bd59c10d5aab15d16988180
Ericson Willians Rocha Pires <ericsonwrp@gmail.com>
2015-10-10 16:56:59 -0300

    Method inclusion and alteration
    Renamed add_css to link_css,
    Added link_js,
    Added add_meta_tags,
    Updated the example.

--- Examples/serv.py
+++ Examples/serv.py
@@ -1,10 +1,11 @@
 from Amazonia.main import WebPage, WebApp
-from paste import httpserver
 
 webpage = WebPage()
-webpage.add_css("css/bootstrap.min.css", "font-awesome/css/font-awesome.min.css")
 
-application = webApp(webpage)
+webpage.link_css("css/bootstrap.min.css", "font-awesome/css/font-awesome.min.css")
+webpage.add_meta_tags(
+	charset=("utf-8",), 
+	name=("viewport", "width=device-width, initial-scale=1"),
+	author=("Ericson Willians",))
 
-# if __name__ == "__main__":
-#	httpserver.serve(WebApp(webpage), host = "192.168.0.77", port = "8080")
+application = WebApp(webpage)
--- main.py
+++ main.py
@@ -23,43 +23,69 @@
 from bs4 import BeautifulSoup
 
 class WebPage(object):
-    
-    def __init__(self, html=""):
-        if html:
-            self.html = html
-        else:
-            self.html = \
-            """
-                <!DOCTYPE html>
-                <html>
-                    <head>
-                        <title></title>
-                    </head>
-                    <body></body>
-                </html>
-            """
-            self.soup = BeautifulSoup(self.html, "lxml")
+	
+	def __init__(self, html=""):
+		if html:
+			self.html = html
+		else:
+			self.html = \
+			"""
+				<!DOCTYPE html>
+				<html>
+					<head>
+						<title></title>
+					</head>
+					<body><h1>Amazonia App</h1></body>
+				</html>
+			"""
+			self.soup = BeautifulSoup(self.html, "lxml")
 
-    def add_css(self, *links):
-        if links:
-            for link in links:
-                new_soup = BeautifulSoup('<link rel="stylesheet", \
-                    href="{external_css}", type="text/css">'.format(external_css = link), "lxml")
-                self.soup.head.insert(0, new_soup.find("link"))
-            self.update_document()
-            
-    def update_document(self):
-        self.html = self.__str__()
-        
-    def __str__(self):
-        return self.soup.prettify()
+	def add_meta_tags(self, **kwargs):
+		if kwargs:
+			for key in kwargs:
+				if len(kwargs[key]) == 2:
+					new_soup = BeautifulSoup('\
+						<meta {attr_name}="{attr_value}" content="{content_value}">'.format(
+							attr_name = key,
+							attr_value = kwargs[key][0],
+							content_value = kwargs[key][1]))
+					self.soup.head.insert(0, new_soup.find("meta"))
+				elif len(kwargs[key]) == 1:
+					new_soup = BeautifulSoup('\
+						<meta {attr_name}="{attr_value}">'.format(
+							attr_name = key,
+							attr_value = kwargs[key][0]))
+					self.soup.head.insert(0, new_soup.find("meta"))
+			self.update_document()
+
+	def link_css(self, *links):
+		if links:
+			for link in links:
+				new_soup = BeautifulSoup('<link rel="stylesheet", \
+					href="{external_css}", type="text/css">'.format(external_css = link), "lxml")
+				self.soup.head.insert(0, new_soup.find("link"))
+			self.update_document()
+	
+	def link_js(self, *links):
+		if links:
+			for link in links:
+				new_soup = BeautifulSoup('<script language="javascript", \
+					src="{external_js}", type="text/javascript"></script>'.format(external_js = link), "lxml")
+				self.soup.head.insert(0, new_soup.find("link"))
+			self.update_document()
+	
+	def update_document(self):
+		self.html = self.__str__()
+		
+	def __str__(self):
+		return self.soup.prettify()
 
 class WebApp(object):
-    
-    def __init__(self, webpage):
-        self.webpage = webpage
-        
-    def __call__(self, environ, start_response):
-        start_response("200 OK", [("Content-type", "text/html")])
-        
-        return [str.encode(self.webpage.html)]
+	
+	def __init__(self, webpage):
+		self.webpage = webpage
+		
+	def __call__(self, environ, start_response):
+		start_response("200 OK", [("Content-type", "text/html")])
+		
+		return [str.encode(self.webpage.html)]
