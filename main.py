0c6c4e08a643308d77f58d56616c873e05c01535
Ericson Willians Rocha Pires <ericsonwrp@gmail.com>
2015-10-22 19:40:17 -0200

    Revert "Present"
    This reverts commit 851f23f4d3a01213bc94ae66ec7c08061e392b73.

--- main.py
+++ main.py
@@ -36,7 +36,6 @@ class ServerResource(object):
 		".jpeg",
 		".png",
 		".gif",
-		".svg",
 		".wav",
 		".mp3",
 		".avi",
@@ -48,11 +47,9 @@ class ServerResource(object):
 			if path.endswith(ext):
 				with open(path, "rb") as f:
 					self.content = f.read()
-					break
 			else:
 				with open(path, 'r', encoding=enc) as f:
 					self.content = f.read()
-					break
 
 class WebPage(object):
 	
@@ -117,13 +114,12 @@ class WebApp(object):
 	MIME_TABLE = {
 		".txt": "text/plain",
 		".css": "text/css",
-		".less": "text/less",
 		".js": "application/javascript",
 		".jpg": "image/jpeg",
 		".png": "image/png"
 	}
 	
-	def __init__(self, root_path=""):
+	def __init__(self, root_path):
 		self.root_path = root_path
 	
 	def __call__(self, environ, start_response):
