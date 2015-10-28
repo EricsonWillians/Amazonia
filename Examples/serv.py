1c565d890fe2092be1feba63e6474d1562cae58a
Ericson Willians Rocha Pires <ericsonwrp@gmail.com>
2015-10-27 22:18:48 -0200

    Updated structure.

--- Examples/serv.py
+++ Examples/serv.py
@@ -1,4 +1,21 @@
 from Amazonia import amazonia
+import pymysql
 
-# Server(WebApp("G:/Insanity/Web/Apache24/htdocs/Volapp"))
-amazonia.Server(amazonia.EnvPrinter())
+root_path = "G:/Insanity/Web/Apache24/htdocs/Volapp"
+connection = connection = pymysql.connect(
+	host='localhost',
+	user='root',
+	password='yourpasshere',
+	db='exampledb',
+	charset='utf8mb4',
+	cursorclass=pymysql.cursors.DictCursor
+) 
+
+class MainApp(amazonia.WebApp):
+	
+	def __call__(self, environ, start_response):
+		self.path_info = environ["PATH_INFO"]
+		
+		return self.get_static_content(self.path_info, start_response)
+
+amazonia.Server(MainApp(root_path))
